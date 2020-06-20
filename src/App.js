3ca7581444c3e7a52e12ac62be1fcdb354833fe0
Grassper <rishi.bharath27@gmail.com>
2020-06-20 19:20:46 +0530

    collection-preview collection-item header components are added

--- src/App.js
+++ src/App.js
@@ -2,22 +2,16 @@ import React from 'react';
 import './App.css';
 import Homepage from "./pages/Homepage/homepage.jsx"
 import {Route,Switch} from "react-router-dom"
-
-const Hatspage = () =>{
-  return(
-    <div>
-    <h1>Hats page.</h1>
-    </div>
-  )
-}
-
+import Shop from "./pages/Shop/shop.component.jsx"
+import Header from "./components/header/Header.component.jsx"
 
 function App() {
   return (
     <div>
+    <Header/>
     <Switch>
       <Route exact path="/" component={Homepage} />
-      <Route path="/shop/hats" component={Hatspage} />
+      <Route path="/shop" component={Shop} />
     </Switch>
     </div>
   );
