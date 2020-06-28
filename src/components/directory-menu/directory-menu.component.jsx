13bdbe8b3a6050478b6ca53996603f003ee31771
Grassper <rishi.bharath27@gmail.com>
2020-06-28 09:59:52 +0530

    directory state and shop state is moved to the redux and reducer for that is created

--- src/components/directory-menu/directory-menu.component.jsx
+++ src/components/directory-menu/directory-menu.component.jsx
@@ -1,59 +1,29 @@
 import React from "react"
-import "./directory-menu.component.scss"
+
+import { connect } from "react-redux";
+
+import { createStructuredSelector } from "reselect";
+import { selectSection } from "../../redux/directory/directory.selector";
+
 import Menuitem from "../menu-item/menu-item.component"
 
-class Directorymenu extends React.Component{
-    constructor(){
-        super()
-        this.state = {
-            section : [
-                {
-                  title: 'hats',
-                  imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-                  id: 1,
-                  linkUrl: 'shop/hats'
-                },
-                {
-                  title: 'jackets',
-                  imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-                  id: 2,
-                  linkUrl: 'shop/jackets'
-                },
-                {
-                  title: 'sneakers',
-                  imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-                  id: 3,
-                  linkUrl: 'shop/sneakers'
-                },
-                {
-                  title: 'womens',
-                  imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-                  size: 'large',
-                  id: 4,
-                  linkUrl: 'shop/womens'
-                },
-                {
-                  title: 'mens',
-                  imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-                  size: 'large',
-                  id: 5,
-                  linkUrl: 'shop/mens'
-                }
-              ]
-        }
-    }
+import "./directory-menu.component.scss"
 
-    render(){
+
+const Directorymenu = ({section}) => {
         return (
             <div className="directory-menu">
-            {this.state.section.map(({id,...otherSectionProps}) => {
+            {section.map(({id,...otherSectionProps}) => {
                 return (
                     <Menuitem key={id} {...otherSectionProps}/>
                 )
             })}
             </div>
         )
-    }
 }
 
-export default Directorymenu;
+const mapStateToProps = createStructuredSelector({
+  section:selectSection
+})
+
+export default connect(mapStateToProps)(Directorymenu);
--- src/pages/Shop/shop.component.jsx
+++ src/pages/Shop/shop.component.jsx
@@ -1,26 +1,27 @@
-import React from "react"
-import "./shop.styles.scss"
-import SHOP_DATA from "./shop.data.js"
-import CollectionPreview from "../../components/collection-preview/collection-preview.component"
+import React from "react";
+
+import { connect } from "react-redux";
+
+import { createStructuredSelector } from "reselect";
+import { selectCollections } from "../../redux/shop/shop.selector";
+
+import CollectionPreview from "../../components/collection-preview/collection-preview.component";
+
+import "./shop.styles.scss";
 
 
-class Shop extends React.Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            collections:SHOP_DATA
-        }
-    }
-    render(){
-        const {collections} = this.state;
-        return (
-            <div className='shop-page'>
-                {collections.map(({id, ...otherCollectionProps})=>(
-                    <CollectionPreview key={id} {...otherCollectionProps}/>
-                ))}
-            </div>
-        )
-    }
+const Shop = ({collections}) => {
+    return (
+        <div className='shop-page'>
+            {collections.map(({id, ...otherCollectionProps})=>(
+                <CollectionPreview key={id} {...otherCollectionProps}/>
+            ))}
+        </div>
+    )
 }
 
-export default Shop
+const mapStateToProps = createStructuredSelector({
+    collections:selectCollections
+})
+
+export default connect(mapStateToProps)(Shop);
