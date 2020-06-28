55289dadac4cf35fbedbc046f3cdc6111d8d9135
Grassper <rishi.bharath27@gmail.com>
2020-06-28 16:32:21 +0530

    logger is removed in heroku production build

--- src/redux/store.js
+++ src/redux/store.js
@@ -5,7 +5,11 @@ import logger from "redux-logger";
 
 import rootReducer from "./root.reducer.js";
 
-const middleware = [logger];
+const middleware = [];
+
+if (process.env.NODE_ENV === "development"){
+    middleware.push(logger);
+}
 
 const store = createStore(rootReducer,applyMiddleware(...middleware));
 
